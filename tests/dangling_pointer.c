ba45f696d6ed6ede2b8d9e023abf32e040096abb
BrianMath <brianmatheus10@gmail.com>
2024-11-13 11:21:46 -0300

    Dangling pointer

--- tests/dangling_pointer.c
+++ tests/dangling_pointer.c
@@ -2,21 +2,34 @@
 #include <stdlib.h>
 
 int main() {
-	int *ptr;
-	int *vet = malloc(1024);
-	ptr = vet;
-	
-	printf("vet = %p\n", vet);
-	printf("ptr = %p\n", ptr);
+	//int *ptr;
+	int *vet = (int*) malloc(2*sizeof(int));
+	printf("Na stack &vet = %p | Valor: %p\n", &vet, vet);
+	//printf("Na stack &ptr = %p | Valor: %p\n", &ptr, ptr);
+	//ptr = vet;
+	printf("ptr = vet\n");
+	printf("Na stack &vet = %p | Valor: %p\n", &vet, vet);
+	//printf("Na stack &ptr = %p | Valor: %p\n", &ptr, ptr);
+
+	vet[1] = 420;
+	printf("vet[1] = %d\n", vet[1]);
 
 	free(vet);
-	ptr[2] = 3;
+	vet = NULL;
+	printf("free(vet)\n");
+	printf("Na stack &vet = %p | Valor: %p\n", &vet, vet);
+	//printf("Na stack &ptr = %p | Valor: %p\n", &ptr, ptr);
 
-	printf("%d\n", *ptr);
+	int *vet1 = (int*) malloc(2*sizeof(int));
+	vet1[1] = 69;
+	//vet[1] = 420;
+	printf("vet[1] = %d\n", vet[1]);
+
+	/*printf("%d\n", *ptr);
 	ptr++;
 	printf("%d\n", *ptr);
 	ptr++;
 	printf("%d\n", *ptr);	// not possible to access memory space that was freed
-
+*/
 	return 0;
 }
